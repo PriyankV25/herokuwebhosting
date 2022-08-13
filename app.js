df27c7f6dc6f7fc623cdb26164f6dc01c7c15cd8
Priyank Saxena <shubhmukul22@gmail.com>
2022-08-14 01:12:00 +0530

    add gitignore, procfile and update port, port missed but done, some update

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ app.get("/work", function(req, res){
 app.post("/work", function(req,res){
   let item = req.body.newItem;
   workItems.push(item);
-  res.redirect("/work")
+  res.redirect("/work");
 });
 app.get("/about", function(req, res){
   res.render("about");
